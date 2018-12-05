b08a1468d8705c38e2a10f8e2d4049ffb6443446
Luciano Suzarti <lucianosuzarti@gmail.com>
2018-12-05 14:27:07 -0300

    add pagina javascript

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+ <!--  JQUERY para controlar os eventos dos botões -->
+
+<script src="http://code.jquery.com/jquery-1.12.0.min.js">
+
+    <script>
+       $(".btn-menu").click(function(){
+       $(".menu").show();
+      });
+        $(".btn-fechar").click(function(){
+        $(".menu").hide();
+      });
+    </script>
