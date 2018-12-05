9324250f025f515ddfcc41fe2850d1e09c25aec7
Luciano Suzarti <lucianosuzarti@gmail.com>
2018-12-05 14:39:31 -0300

    exclusão do gitignore e funções.js

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,9 @@
  <!--  JQUERY para controlar os eventos dos botões -->
 
-<script src="http://code.jquery.com/jquery-1.12.0.min.js">
+<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
 
     <script>
-       $(".btn-menu").click(function(){
+       Document.getElementById(".btn-menu").click(function(){
        $(".menu").show();
       });
         $(".btn-fechar").click(function(){
