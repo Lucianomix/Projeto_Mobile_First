3296348036f17e5fac3f1f8105484691ef494878
Luciano Suzarti <lucianosuzarti@gmail.com>
2018-12-13 14:55:06 -0300

    melhorias nas paginas 13-12 14:54

--- funcoes.js
+++ funcoes.js
@@ -1,10 +1,21 @@
- <!--  JQUERY para controlar os eventos dos botões -->
 
-    <script>
+<!--  JQUERY para controlar os eventos dos botões -->
+
        document.getElementById(".btn-menu").click(function(){
        $(".menu").show();
       });
         $(".btn-fechar").click(function(){
         $(".menu").hide();
       });
-    </script>
+
+<!--Função para contar os caracteres das mensagens-->
+
+
+    function contaCaracteres(campoPassado){
+      var limite = 500;
+      var campo = campoPassado.value.length;
+      var maxlenght = document.getElementById("campoTexto").setAttribute("maxlenght", limite);
+      var divContagem = document.getElementById("divContagem");
+      var restante = limite - campo;
+      divContagem.innerHTML = restante;
+}
